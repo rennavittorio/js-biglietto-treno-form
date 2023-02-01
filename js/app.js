29e24740c1926b27c9eefcb579f2da3a3e8dc980
rennavittorio <renna.vittorio@gmail.com>
2023-02-01 18:19:15 +0100

    ADD js algorithm updated to show user data on screen

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let fullNameElement, distanceRouteElement, ageClusterElement;
 const priceAtKm = 0.21;
 let ticketPrice;
 
+//richiamare elementi per inserimento dati utente
+let userNameOutput = document.getElementById('userNameOutput');
+let ticketTypeOutput = document.getElementById('ticketTypeOutput');
+let ticketPriceOutput = document.getElementById('ticketPriceOutput');
+// console.log(userNameOutput, ticketTypeOutput, ticketPriceOutput);
+
 
 //richiamare bottone generator
 const generateTicketElement = document.querySelector('.generateTicket');
@@ -18,7 +24,7 @@ generateTicketElement.addEventListener('click', function(){
     distanceRouteElement = document.querySelector('.distanceRoute').value;
     distanceRouteElement = parseFloat(distanceRouteElement);
     ageClusterElement = document.querySelector('.ageCluster').value;
-    console.log('dati utente sono', fullNameElement, distanceRouteElement, ageClusterElement);
+    // console.log('dati utente sono', fullNameElement, distanceRouteElement, ageClusterElement);
 
     //calcolare prezzo biglietto base (km * 0.21)
     ticketPrice = distanceRouteElement * priceAtKm;
@@ -30,18 +36,26 @@ generateTicketElement.addEventListener('click', function(){
         //ALTRIMENTI nessuno sconto
     if (ageClusterElement === 'under18') {
         ticketPrice = ticketPrice * .80;
+        ticketTypeOutput.innerHTML = 'Biglietto Junior';
 
     } else if (ageClusterElement === 'over65'){
         ticketPrice = ticketPrice * .60;
+        ticketTypeOutput.innerHTML = 'Biglietto Senior';
 
-    } else {
+    } else if (ageClusterElement === 'adult') {
         ticketPrice = ticketPrice;
-    }
+        ticketTypeOutput.innerHTML = 'Biglietto Standard';
+    } 
+
+    //inserire dati utente in elementi ticket
+    userNameOutput.innerHTML = fullNameElement;
 
     //Mostrare dati inseriti e prezzo finale a utente
     ticketPrice = parseFloat(ticketPrice).toFixed(2);
     ticketPrice = parseFloat(ticketPrice);
     console.log('prezzo scontato', ticketPrice);
+
+    ticketPriceOutput.innerHTML = ticketPrice;
 })
 
     
